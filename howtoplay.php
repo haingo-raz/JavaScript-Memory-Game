da96243aae54a1838a6597fa28b63ae77f006117
Haingo <haingo.razafindranto@gmail.com>
2024-08-20 16:32:09 -0400

    Rules // Update text

--- howtoplay.php
+++ howtoplay.php
@@ -1,35 +1,35 @@
 <?php
-    //link to the external php file
+    // Link to the external php file
     include 'common.php';
 
-    //ouput header
+    // Output header
     outputHeader("How To Play");
 
-    //output banner at the top of the page-->
+    // Output banner at the top of the page
     outputBanner();
 
-    //output navigation  bar
+    // Output navigation bar
     outputNavBar("How To Play");
 ?>
 
 
 <div class="rules-container">
-    <!--title-->
+    <!-- Title -->
     <h2>Rules of the game:</h2>
 
-    <!--Display instructions for the game in bullet points-->
+    <!-- Display instructions for the game in bullet points -->
     <ul id="tutorials">
         <li><a href="registration.php">Register</a> with your details.</li>
         <li><a href="login.php">Login</a> to your account with your username and password to save highscores.</li>
-        <li>Go on the <a href="index.php">game</a> page.</li>
-        <li>Click on the play button to play the game.</li>
-        <li>The game has 3 different levels (Easy / Medium / Difficult) and you have to play all 3 at once. The 3 levels have respectively 8,12 and 16 cards.</li>
-        <li>Find all the pairs by clicking on the cards and combine them together.</li>
-        <li>You will receive a score of 10 points for each right pair guess.</li>
-        <li>Complete the game within the time limit (2:00mn).</li>
-        <li>Your final score will be stored if you complete the game within the time limit or not.</li>
-        <li>Check your score in the <a href="rankings.php">rankings</a> page.</li>
-        <li>You can logout in the <a href="index.php">game</a> page by clicking on the logout link in the top right corner</li>
+        <li>Go to the <a href="index.php">game</a> page.</li>
+        <li>Click on the play button to start the game.</li>
+        <li>The game has 3 different levels (Easy / Medium / Difficult) and you have to play all 3 at once. The 3 levels have 8, 12, and 16 cards respectively.</li>
+        <li>Find all the pairs by clicking on the cards and matching them together.</li>
+        <li>You will receive a score of 10 points for each correct pair guess.</li>
+        <li>Complete the game within the time limit (2:00 minutes).</li>
+        <li>Your final score will be stored whether you complete the game within the time limit or not.</li>
+        <li>Check your score on the <a href="rankings.php">rankings</a> page.</li>
+        <li>You can logout on the <a href="index.php">game</a> page by clicking on the logout link in the top right corner.</li>
     </ul>
 </div>
 
